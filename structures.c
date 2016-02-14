acc600a83252f4abedc91a1002bdddf79ed198d7
Gaurav Grover <gauravgrover95@gmail.com>
2016-02-14 19:18:47 +0530

    structures basic program

--- structures.c
+++ structures.c
@@ -1,24 +1,55 @@
-#include "stdio.h"
-
+#include <stdio.h>
+#include <math.h>
 
 struct point {
-	int x;
-	int y;
+    int x;
+    int y;
 };
 
+struct rectangle {
+
+    struct point leftbot;
+    struct point righttop;
+};
+
+// funct to make the point
 struct point makePoint(int x, int y) {
 
-	struct point temp;
-	temp.x = x;
-	temp.y = y;
-	return temp;
+} 
+
+// func to calculate distance from origin
+float dist(struct point temp) {
+    int X = temp.x;
+    int Y = temp.y;
+    float dista = sqrt(X*X + Y*Y);
+    return dista;
 }
 
+// function to calculate distance between two points
+float distance(struct point a, struct point b) {
+
+    float dista = sqrt((b.y - a.y)*(b.y - a.y) + (b.x - a.x)*(b.x - a.x));
+    return dista;
+}
+
+// function to calculate area of the rectangle
+float areaOfRectangle(struct rectangle temp) {
+    int length = temp.righttop.x - temp.leftbot.x;
+    // printf("distance betwsdfjbvjkdf: %f\n", length);
+    float diagnol = distance(temp.righttop, temp.leftbot);
+    float Area = length * sqrt(diagnol*diagnol - length*length);
+    return Area;
+}
+
+
 int main() {
-	struct point pt;
-	pt.x = 1;
-	pt.y = 2;
-	printf("x and y co-ordinates are: %d, %d\n", pt.x, pt.y);
-	pt = makePoint(5, 6);
-	printf("x and y co-ordinates are: %d, %d\n", pt.x, pt.y);
+    printf("Hello\n");
+    struct point pt;
+    pt.x = 1;
+    pt.y = 1;
+    float dista = dist(pt);
+    printf("Distance from origin is: %.3f\n", dista);
+    struct rectangle myRectangle = {{0, 0}, {1,1}};
+    float area = areaOfRectangle(myRectangle);
+    printf("\nArea of my rectangle is: %.3f\n", area);
 }
